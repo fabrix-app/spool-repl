2e51e42f0ae8f3fee6f3f2989a344b7f1d1414a9
scott-wyatt <scottwyatt86@gmail.com>
2018-06-22 12:58:06 -0400

    [chore] standardize

--- lib/config/repl.ts
+++ lib/config/repl.ts
@@ -1,3 +1,10 @@
 export const repl = {
+  // default encoding of http request response
+  httpEncoding: 'utf8',
+
+  // default history file name
+  historyFileName: '.node_repl_history',
+
+  // Allow No TTY
   allowNoTTY: false
 }
